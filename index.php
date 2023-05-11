4889d9eecf3cceb90c4140fed23ee0031592cab9
natsuki <natsuki.uji410@gmail.com>
2023-05-11 23:09:59 +0900

    課題完了しました

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-testphp
+<?php
+$question["問題"] = "日本の首都は？";
+$answer["回答1"] = "大阪";
+$answer["回答2"] = "北海道";
+$answer["回答3"] = "箱根";
+$answer["回答4"] = "東京";
+echo $question["問題"] . "<br>";
+foreach($answer as $key => $value) {
+    echo $key . ": " . $value . "<br>";
+}
+?>
